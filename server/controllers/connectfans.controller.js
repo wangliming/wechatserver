5b77c96309f52be0810a6cb4c6db48b29a2ad0ff
wangliming <wangliming05231105@126.com>
2017-11-13 09:59:23 +0800

    by wangliming

--- server/controllers/connectfans.controller.js
+++ server/controllers/connectfans.controller.js
@@ -2,7 +2,7 @@ var events = require("events");
 var fansemitter = require('./../helpers/uploadfansinfo')
 
 function saybyname(req, res, next, id){
-  console.log(req);
+  // console.log(req);
   fansemitter.emit("saybyname", req.body);
 }
 exports.default = { saybyname: saybyname};
--- server/helpers/uploadfansinfo.js
+++ server/helpers/uploadfansinfo.js
@@ -1,7 +1,7 @@
 var events = require("events");
 var fansemitter = new events.EventEmitter();
 
-fansemitter.addListener("saybyname",function(){
-    console.log("事件触发，调用此回调函数");
-});
+// fansemitter.addListener("saybyname",function(){
+//     console.log("事件触发，调用此回调函数");
+// });
 module.exports = fansemitter;
