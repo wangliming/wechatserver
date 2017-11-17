94dd2614f719fe7ea234714012c96df3ce7575de
wangliming <wangliming05231105@126.com>
2017-11-17 12:31:14 +0800

    by wangliming

--- server/controllers/connectfans.controller.js
+++ server/controllers/connectfans.controller.js
@@ -1,9 +1,23 @@
 var events = require("events");
 var fansemitter = require('./../helpers/fansemitter')
-
-function saybyname(req, res, next, id){
+// 从web客户端向虚拟机粉丝端发消息
+function fromsaybyname(req, res, next, id){
   // console.log(req);
-  fansemitter.emit("saybyname", req.body);
+  fansemitter.emit("fromsaybyname", req.body);
 }
-exports.default = { saybyname: saybyname};
+function fromchatinroom(req, res, next, id){
+  // 在群里聊天
+  fansemitter.emit("fromchatinroom", req.body);
+}
+// 虚拟机粉丝端向web客户端向发消息
+function tosaybyname(req, res, next, id){
+  // console.log(req);
+  // fansemitter.emit("saybyname", req.body);
+}
+function tochatinroom(req, res, next, id){
+  // 在群里聊天
+  // fansemitter.emit("chatinroom", req.body);
+}
+
+exports.default = { fromsaybyname: fromsaybyname, fromchatinroom: fromchatinroom, tosaybyname: tosaybyname, tochatinroom: tochatinroom};
 module.exports = exports['default'];
--- server/routes/connectfans.route.js
+++ server/routes/connectfans.route.js
@@ -5,8 +5,12 @@ var fansCtrl = require('../controllers/connectfans.controller')
 
 var router = express.Router(); // eslint-disable-line new-cap
 
-router.route('/saybyname/:toFansName')
-  /** POST /api/saybyname/:toFansName - 根据粉丝姓名找到粉丝并进行聊天 */
-  .post(fansCtrl.saybyname);
-router.param('toFansName', fansCtrl.saybyname);
+router.route('/fromsaybyname/')
+  /** POST /api/saybyname/:toFansName - 根据粉丝姓名找到粉丝并进行聊天（从web客户端向粉丝端） */
+  .post(fansCtrl.fromsaybyname);
+// 建群聊天
+// 在群里聊天
+// 从群里踢人
+// 删除联系人
+router.param('/tosaybyname/', fansCtrl.tosaybyname);
 module.exports = router
