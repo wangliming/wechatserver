ca71e04f937b4cdb8f5b5691fc1e780e297ec137
wangliming <wangliming05231105@126.com>
2017-11-17 14:16:44 +0800

    by wangliming

--- server/controllers/connectfans.controller.js
+++ server/controllers/connectfans.controller.js
@@ -1,5 +1,6 @@
 var events = require("events");
 var fansemitter = require('./../helpers/fansemitter')
+var httphelper = require('./../helpers/httphelper')
 // 从web客户端向虚拟机粉丝端发消息
 function fromsaybyname(req, res, next, id){
   // console.log(req);
@@ -10,14 +11,13 @@ function fromchatinroom(req, res, next, id){
   fansemitter.emit("fromchatinroom", req.body);
 }
 // 虚拟机粉丝端向web客户端向发消息
-function tosaybyname(req, res, next, id){
-  // console.log(req);
-  // fansemitter.emit("saybyname", req.body);
-}
-function tochatinroom(req, res, next, id){
-  // 在群里聊天
-  // fansemitter.emit("chatinroom", req.body);
-}
+fansemitter.addListener('tosaybyname', async param => {
+  // 发送http请求--wechaty请求
+
+})
+fansemitter.addListener('tochatinroom', async param => {
+  // wechat请求
+})
 
 exports.default = { fromsaybyname: fromsaybyname, fromchatinroom: fromchatinroom, tosaybyname: tosaybyname, tochatinroom: tochatinroom};
 module.exports = exports['default'];
